87fede45368374f726b3822e543386a96f4a5611
Brooks Susor <susorbrooks@gmail.com>
2022-09-08 13:20:55 -0400

    Not very good code

--- scripts.js
+++ scripts.js
@@ -1,10 +1,29 @@
 const ticTaceToe = (() => {
-  const gameBoard = { arr: [] }; 
-  const displayController = {}; 
-  return { gameBoard, displayController }; 
-})(); 
-
-const player = name => {
+  let divBoard = document.createElement('div'); 
+  const gameBoard = { 
+    arr: [
+      'X', ' ', 'X', 
+      'O', 'O', ' ', 
+      'X', 'O', 'X'
+    ] }; 
+    const generateGameboard = () => { 
+      for (let i = 0; i < gameBoard.arr.length; i++) {
+        divBoard.textContent += gameBoard.arr[i]; 
+        if ((i + 1) % 3 === 0 && (i + 1) % 2 === 0) {
+          document.body.appendChild(divBoard); 
+          divBoard = document.createElement('div'); 
+        }
+      }
+    } 
+    return { generateGameboard }; 
+  })(); 
+  
+const Player = name => {
   const getName = () => name; 
   return { getName }; 
 }
+
+const Brooks = Player('Brooks'); 
+const Jake = Player('Jake'); 
+
+ticTaceToe.generateGameboard(); 
