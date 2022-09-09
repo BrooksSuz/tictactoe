e49f603e221cf28128bdb731a9811eb4b24ee6be
Brooks Susor <susorbrooks@gmail.com>
2022-09-09 11:55:09 -0400

    Lots of work

--- scripts.js
+++ scripts.js
@@ -1,21 +1,18 @@
+// CHECK WHICH GIT BRANCH YOU'RE ON 
+
+
+
 const ticTaceToe = (() => {
-  let divBoard = document.createElement('div'); 
   const gameBoard = { 
     arr: [
-      'X', ' ', 'X', 
-      'O', 'O', ' ', 
+      'X', 'O', 'X', 
+      'O', 'O', 'X', 
       'X', 'O', 'X'
     ] }; 
-    const generateGameboard = () => { 
-      for (let i = 0; i < gameBoard.arr.length; i++) {
-        divBoard.textContent += gameBoard.arr[i]; 
-        if ((i + 1) % 3 === 0 && (i + 1) % 2 === 0) {
-          document.body.appendChild(divBoard); 
-          divBoard = document.createElement('div'); 
-        }
-      }
-    } 
-    return { generateGameboard }; 
+
+    
+
+    return {  }; 
   })(); 
   
 const Player = name => {
@@ -26,4 +23,4 @@ const Player = name => {
 const Brooks = Player('Brooks'); 
 const Jake = Player('Jake'); 
 
-ticTaceToe.generateGameboard(); 
+ticTaceToe.createGameBoard(); 
