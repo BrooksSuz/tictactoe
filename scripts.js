c01571327457df3e4e36141f4c0cd63fd2edef8e
Brooks Susor <susorbrooks@gmail.com>
2022-09-12 10:46:21 -0400

    Create winGame horizontal function

--- scripts.js
+++ scripts.js
@@ -19,15 +19,30 @@ const ticTaceToe = (() => {
   
 const Player = (name) => {
   const getName = () => name; 
+  
+  const divCells = document.querySelectorAll('div[class^=cell]'); 
+
+  const _winGame = () => {
+    // horizontal wins
+    if (divCells[0].textContent === 
+      (divCells[1].textContent && divCells[2].textContent) && 
+      divCells[0].textContent === 'X') {
+      alert('you win!'); 
+    } else if (divCells[3].textContent === (divCells[4].textContent && divCells[5].textContent) && divCells[3].textContent === 'X') {
+      alert('you win!'); 
+    } else if (divCells[6].textContent === (divCells[7].textContent && divCells[8].textContent) && divCells[6].textContent === 'X') {
+      alert('you win!'); 
+    }
+  }
 
   const addMark = () => { 
-    const divCells = document.querySelectorAll('div[class^=cell]'); 
     for (let i = 0; i < divCells.length; i++) { 
       divCells[i].addEventListener('click', () => {
         if (divCells[i].textContent === '') {
           divCells[i].classList.add('cells'); 
           divCells[i].textContent += 'X'; 
         }
+        _winGame(); 
       });
     }
   }
